3f6da8eaf91d133f2df9af700631a7f425b71c61
bktpp <117415722+bktpp@users.noreply.github.com>
2023-11-30 22:37:13 -0600

    started tic tac toe logic and functionality

--- src/games/tic-tac-toe/tic-tac-toe.component.jsx
+++ src/games/tic-tac-toe/tic-tac-toe.component.jsx
@@ -1,12 +1,74 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import winningCombos from "./winning-combos";
+
+import "./tic-tac-toe.styles.scss";
 
 function TicTacToe() {
+   const [tileValues, setTileValues] = useState(Array(9).fill(""));
+   const [isPlayersTurn, setIsPlayersTurn] = useState(true);
+   const [pickedTiles, setPickedTiles] = useState([]);
+   // const [userPicks, setUserPicks] = useState([Array(3).fill(""), Array(3).fill("")]);
+   const [userPicks, setUserPicks] = useState([]);
+   const [computerPicks, setComputerPicks] = useState([]);
+
+   const check = [1, 2, 3];
+
+   function checkWinner(picks) {
+      console.log(picks);
+      const isEqual = check.every((value, index) => value === picks[index]);
+
+      if (isEqual) {
+         alert("congrats you won");
+      } else {
+         console.log("no winner");
+      }
+   }
+
+   function handleClick(index) {
+      if (pickedTiles.includes(index)) {
+         alert("spot already picked. try again");
+      } else if (!pickedTiles.includes(index)) {
+         setTileValues((previousValues) => {
+            const newValues = [...previousValues];
+            if (isPlayersTurn) {
+               newValues[index] = "X";
+               setUserPicks((picks) => {
+                  const updatedPicks = [...picks, index + 1];
+                  checkWinner(updatedPicks);
+                  return updatedPicks;
+               });
+               setIsPlayersTurn(!isPlayersTurn);
+            } else if (!isPlayersTurn) {
+               newValues[index] = "O";
+               setIsPlayersTurn(!isPlayersTurn);
+            }
+            setPickedTiles((tiles) => [...tiles, index]);
+            return newValues;
+         });
+      }
+   }
+
+   function handleClearBoard() {
+      setIsPlayersTurn(true);
+      setTileValues(Array(9).fill(""));
+      setPickedTiles([]);
+   }
+
+   const tiles = tileValues.map((value, index) => (
+      <div key={index} className="tile" onClick={() => handleClick(index)}>
+         <div className="tile-content">{value}</div>
+      </div>
+   ));
+
    return (
-      <div>
-         <h1> tic tac toe will be here</h1>
+      <div className="main-container">
          <Link to={"/"}>
             <button>Home</button>
          </Link>
+         <h1> Tic Tac Toe</h1>
+         <div className="gameboard-container">{tiles}</div>
+         <button onClick={handleClearBoard}>Reset</button>
       </div>
    );
 }
